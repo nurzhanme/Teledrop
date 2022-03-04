d826d32db725999a91f3f3ae368c9974f442d06b
Nurzhan <nurzhait@gmail.com>
2022-03-04 16:55:34 +0600

    add Auth attribute to InstaAccount controller

--- Teledrop/Controllers/InstaAccountsController.cs
+++ Teledrop/Controllers/InstaAccountsController.cs
@@ -1,4 +1,5 @@
 ﻿#nullable disable
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Teledrop.Models;
@@ -6,6 +7,7 @@ using Teledrop.Services;
 
 namespace Teledrop.Controllers
 {
+    [Authorize]
     public class InstaAccountsController : Controller
     {
         private readonly TeledropDbContext _context;
